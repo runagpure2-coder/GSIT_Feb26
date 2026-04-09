045bf7677bda5c8d43ec41a0686abc57a4b3c6f3
runagpure2-coder <runagpure2@gmail.com>
2026-04-09 16:22:53 +0530

    Assignment_7.java

--- Assignment_7.java
+++ Assignment_7.java
@@ -1,22 +1,29 @@
 package assignment;
 
 public class Assignment_7 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4}; 
+
+        boolean isSorted = true; 
+
+        // check ascending order
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+                break; 
+            }
+        }
+
+        if (isSorted) {
+            System.out.println("Array is sorted in ascending order.");
+        } else {
+            System.out.println("Array is NOT sorted.");
+        }
+    }
+}
+
+	    
 	
-	    public static void main(String[] args) {
-	        int[] arr = {1, 2, 3, 4}; // try {1, 3, 2} also
-
-	        boolean result = isSorted(arr);
-	        System.out.println("Is array sorted? " + result);
-	    }
-
-	    static boolean isSorted(int[] arr) {
-	        for (int i = 0; i < arr.length - 1; i++) {
-	            if (arr[i] > arr[i + 1]) {
-	                return false; // not ascending
-	            }
-	        }
-	        return true; // ascending
-	    }
-	}
 
 
